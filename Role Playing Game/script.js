1aaf2b67790aedce73c6779a8e24e2c698e18ca9
Himanshu Rai <himanshuraimau0@gmail.com>
2024-02-16 01:50:42 +0530

    a short update

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -97,13 +97,13 @@ button3.onclick = fightDragon;
 
 function update(location) {
   monsterStats.style.display = "none";
-  button1.innerText = location["button text"][0];
-  button2.innerText = location["button text"][1];
-  button3.innerText = location["button text"][2];
+  button1.innerHTML = location["button text"][0];
+  button2.innerHTML = location["button text"][1];
+  button3.innerHTML = location["button text"][2];
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
 
 function goTown() {
